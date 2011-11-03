ce0c121c222a132c91a5261ecad0a38bcdb4c516
zhck <zhck@mail.ru>
2011-11-03 12:42:37 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DamoApp::Application.config.secret_token = '31a69c9b136ce426a171f79e76dca90508f318f951bae5e78b75bb9bcf4828691859fd3e80426e939c712e1ece3fd3bca1bd491b92188932cd76064876baaa0b'
